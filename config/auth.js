29ea9f9a1f14813d55e6e6c26055c48b54a98d61
jordanbisato <jordanbisato@hotmail.com>
2017-05-25 17:29:28 -0300

    login promo 2.0 update

--- config/auth.js
+++ config/auth.js
@@ -9,7 +9,6 @@ module.exports = {
         'consumerKey': 'pKnfaRfHUNNqGTrsSoPmxS9XA',
         'consumerSecret': 'XouAUtJwv6Saa4su6QaRDLWxYiYYdxftkWU2DI9uNWsj0jjzYa',
         'callbackURL': 'http://www.navega.gratis:8085/login-promo/auth/twitter/callback',
-        'userProfileURL': "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true",
         'includeEmail': true
     },
 
