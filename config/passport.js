b61346468420bb9aea27319d5252a1890b9105bb
jordanbisato <jordanbisato@hotmail.com>
2017-05-26 11:25:09 -0300

    get email from twitter

--- config/passport.js
+++ config/passport.js
@@ -97,7 +97,6 @@ module.exports = function(passport) {
             consumerSecret: configAuth.twitterAuth.consumerSecret,
             callbackURL: configAuth.twitterAuth.callbackURL,
             userProfileURL: "https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true",
-            passReqToCallback: true,
             includeEmail: true
         },
         function(token, tokenSecret, profile, done) {
